cf235c1c36eb4a7a14bac052ede1acc1f17fb34b
OscarSah <oscar@cybertekschool.com>
2021-12-04 23:44:43 +0300

    Zip Task added

---
+++ src/test/java/apiReviews/w12_04_21/ZipTask.java
@@ -0,0 +1,62 @@
+package apiReviews.w12_04_21;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+import static io.restassured.RestAssured.*;
+public class ZipTask {
+    /*
+Given Accept application/json
+And path zipcode is 22031
+When I send a GET request to /us endpoint
+Then status code must be 200
+And content type must be application/json
+And Server header is cloudflare
+And Report-To header exists
+And body should contains following information
+    post code is 22031
+    country  is United States
+    country abbreviation is US
+    place name is Fairfax
+    state is Virginia
+    latitude is 38.8604
+     */
+
+@BeforeClass
+    public void setUp(){baseURI = "http://api.zippopotam.us/";}
+
+    @Test
+    public void test(){
+
+        Response response = given().accept(ContentType.JSON).when().get("US/22031");
+
+        assertEquals(response.statusCode(),200);
+
+      //  And content type must be application/json
+        assertEquals(response.contentType(),"application/json");
+     //  And Server header is cloudflare
+        assertTrue(response.header("Server").equalsIgnoreCase("cloudflare"));
+      //  And Report-To header exists
+        assertTrue(response.headers().hasHeaderWithName("Report-To"));
+      //  And body should contains following information
+     //    post code is 22031
+
+        JsonPath jsonPath = response.jsonPath();
+
+        String postCode = jsonPath.getString("\'post code\'");
+        assertEquals(postCode,"22031");
+ /*
+        country  is United States
+        country abbreviation is US
+        place name is Fairfax
+        state is Virginia
+        latitude is 38.8604
+*/
+
+    }
+
+}

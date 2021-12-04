ead98ca041f9f6f50f36e882142330cee8d91653
OscarSah <oscar@cybertekschool.com>
2021-12-04 19:44:06 +0300

    Review Test Three and Four

--- src/test/java/apiReviews/w12_04_21/R01_API_Test.java
+++ src/test/java/apiReviews/w12_04_21/R01_API_Test.java
@@ -1,15 +1,19 @@
 package apiReviews.w12_04_21;
 
+import static io.restassured.RestAssured.*;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import static org.testng.Assert.*;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class R01_API_Test {
 
     String hrurl = "http://54.92.248.102:1000/ords/hr";
-    String zipUrl = "http://api.zippopotam.us/US/";
+    String zipUrl = "http://api.zippopotam.us/";
     String spartanUrl = "http://54.92.248.102:8000";
 
     // get me all employees from HR api
@@ -21,8 +25,8 @@ public class R01_API_Test {
 
         // response.prettyPrint();
 
-        Assert.assertEquals(response.statusCode(),200);
-        Assert.assertEquals(response.contentType(),"application/json");
+        assertEquals(response.statusCode(),200);
+        assertEquals(response.contentType(),"application/json");
     }
 
     // get me all regions from HR api     hrurl+"/regions"
@@ -35,16 +39,37 @@ public class R01_API_Test {
 
         // response.prettyPrint();
 
-        Assert.assertEquals(response.statusCode(),200);
-        Assert.assertEquals(response.contentType(),"application/json");
+        assertEquals(response.statusCode(),200);
+        assertEquals(response.contentType(),"application/json");
         // does the response have "Europe" in it?
-        Assert.assertTrue(response.body().asString().contains("Europe"));
+        assertTrue(response.body().asString().contains("Europe"));
         //does my response have the header name "Date" in it
-        Assert.assertTrue(response.headers().hasHeaderWithName("Date"));
+        assertTrue(response.headers().hasHeaderWithName("Date"));
         // Transfer-Encoding  = chunked
-        Assert.assertEquals(response.header("Transfer-Encoding"),"chunked");
+        assertEquals(response.header("Transfer-Encoding"),"chunked");
 
     }
+
+    // do a request to Zip Url and get 45414 , verify it contains Dayton, verify status code and contentType
+
+    @Test
+    public void testThree(){
+        Response response = given().accept(ContentType.JSON)   // static import
+                        .when().get(zipUrl+"US/45414");
+
+        assertEquals(response.statusCode(),200);
+        assertEquals(response.contentType(),"application/json");
+        assertTrue(response.body().asString().contains("Dayton"));
+        response.prettyPrint();
+    }
+    @Test
+    public void testFour(){
+        given().accept(ContentType.JSON).when().get(zipUrl+"US/45414")
+                .then().assertThat().contentType("application/json").and().statusCode(200);
+
+
+    }
+
 
 
 

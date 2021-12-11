8541622c2fd6e07f25342113e2b09b7f5ff61206
OscarSah <oscar@cybertekschool.com>
2021-12-11 19:07:36 +0300

    Week 3 Hamcrest Example 2 added

--- src/test/java/apiReviews/w12_11_21/HamcrestTest.java
+++ src/test/java/apiReviews/w12_11_21/HamcrestTest.java
@@ -20,6 +20,8 @@ public class HamcrestTest {
 }
      */
     String spartanUrl = "http://3.239.148.14:8000";
+    String zipUrl = "http://api.zippopotam.us/";  // zipUrl + "US/{zipCode}, 45414
+
     @Test
     public void oneSpartanTest(){
 
@@ -32,7 +34,41 @@ public class HamcrestTest {
          .body("id",equalTo(7),"name",equalTo("Hershel")
          ,"gender",equalTo("Male"),"phone",equalTo(5278678322L));
 
+ }
+/*
+{
+    "post code": "45414",
+    "country": "United States",
+    "country abbreviation": "US",
+    "places": [
+        {
+            "place name": "Dayton",
+            "longitude": "-84.2024",
+            "state": "Ohio",
+            "state abbreviation": "OH",
+            "latitude": "39.8285"
+        }
+    ]
+}
+ */
+
+    @Test
+    public void zipTest(){
+
+        given().accept(ContentType.JSON)
+                .and().pathParam("zipCode",45414)
+                .when().get(zipUrl+"US/{zipCode}")
+                .then().assertThat().statusCode(200)
+                .body("country",equalTo("United States"),
+                        "places.state[0]",equalTo("Ohio"),
+                    "'post code'",equalTo("45414"),
+                        "'country abbreviation'",equalTo("US"));
+
+
+
+
 
     }
+
 
 }

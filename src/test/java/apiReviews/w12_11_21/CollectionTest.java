d4e6d3158ab2dc84b23677c56461ef5b9b67913f
OscarSah <oscar@cybertekschool.com>
2021-12-11 19:40:43 +0300

    Week 3 Collection Example 2 added

--- src/test/java/apiReviews/w12_11_21/CollectionTest.java
+++ src/test/java/apiReviews/w12_11_21/CollectionTest.java
@@ -5,6 +5,7 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -41,9 +42,51 @@ public class CollectionTest {
         assertEquals(spartan7.get("gender"),"Male");
         assertEquals(spartan7.get("phone"),5278678322L);
 
+    }
+
+    /*
+{
+    "post code": "45414",
+    "country": "United States",
+    "country abbreviation": "US",
+    "places": [
+        {
+            "place name": "Dayton",
+            "longitude": "-84.2024",
+            "state": "Ohio",
+            "state abbreviation": "OH",
+            "latitude": "39.8285"
+        }
+    ]
+}
+ */
+    @Test
+    public void zipTestCollection(){
+
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("zipCode",45414)
+                .when().get(zipUrl+"US/{zipCode}");
+
+        assertEquals(response.statusCode(),200);
+
+        // to reach my response body and for assertions
+        Map<String,Object> pc45414 = response.body().as(Map.class);  // deserialization
+
+        System.out.println(pc45414);
+
+        assertEquals(pc45414.get("country"),"United States");
+        assertEquals(pc45414.get("post code"),"45414");
+
+
+        // I am getting "places" key from my Map and putting the value in a List<Map>
+        List<Map<String,Object>> placesFor45414 = (List<Map<String, Object>>) pc45414.get("places");
+
+        assertEquals(placesFor45414.get(0).get("state"),"Ohio");
+        assertEquals(placesFor45414.get(0).get("state abbreviation"),"OH");
 
 
     }
+
 
 
 }

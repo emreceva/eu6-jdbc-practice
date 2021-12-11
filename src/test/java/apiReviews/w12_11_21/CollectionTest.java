2dc0bb563d96e964cdb4f7ae4688a30cb1c0bced
OscarSah <oscar@cybertekschool.com>
2021-12-11 19:22:32 +0300

    Week 3 Collection Example 1 added

--- src/test/java/apiReviews/w12_11_21/CollectionTest.java
+++ src/test/java/apiReviews/w12_11_21/CollectionTest.java
@@ -1,7 +1,49 @@
 package apiReviews.w12_11_21;
 
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.*;
+
 public class CollectionTest {
 
+    String spartanUrl = "http://3.239.148.14:8000";
+    String zipUrl = "http://api.zippopotam.us/";  // zipUrl + "US/{zipCode}, 45414
+
+    @Test
+    public void oneSpartanWithMap(){
+        /*
+    {
+    "id": 7,
+    "name": "Hershel",
+    "gender": "Male",
+    "phone": 5278678322
+}
+     */
+
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id",7)
+                .when().get(spartanUrl+"/api/spartans/{id}");
+
+        assertEquals(response.statusCode(),200);
+
+        Map<String,Object> spartan7 = response.body().as(Map.class);
+
+        System.out.println(spartan7);
+
+        assertEquals(spartan7.get("id"),7);
+        assertEquals(spartan7.get("name"),"Hershel");
+        assertEquals(spartan7.get("gender"),"Male");
+        assertEquals(spartan7.get("phone"),5278678322L);
+
+
+
+    }
 
 
 }
